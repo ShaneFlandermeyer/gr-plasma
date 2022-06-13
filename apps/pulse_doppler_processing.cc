fc544aec8a18f956e3a2e8aba2cfa47bf9265176
ShaneFlandermeyer <shaneflandermeyer@gmail.com>
2022-06-13 12:48:08 -0500

    Fixed file read function

--- apps/pulse_doppler_processing.cc
+++ apps/pulse_doppler_processing.cc
@@ -5,11 +5,12 @@
 std::vector<std::complex<float>> read(const std::string& filename, size_t offset = 0) {
   std::ifstream infile(filename, std::ios::in | std::ios::binary);
   infile.seekg(0, std::ios::end);
-  std::size_t size = infile.tellg();
+  std::size_t nbytes = infile.tellg();
+  std::size_t nitems = nbytes / sizeof(std::complex<float>);
   infile.seekg(offset, std::ios::beg);
   
-  std::vector<std::complex<float>> data(size / sizeof(std::complex<float>));
-  infile.read(reinterpret_cast<char*>(data.data()), size * sizeof(float));
+  std::vector<std::complex<float>> data(nitems);
+  infile.read(reinterpret_cast<char*>(data.data()), nbytes);
   infile.close();
   return data;
 }
@@ -21,12 +22,11 @@ int main(int argc, char *argv[]) {
   double samp_rate = 100e6;
   plasma::LinearFMWaveform waveform(bandwidth, pulse_width, 0, samp_rate);
   Eigen::ArrayXcf mf = waveform.MatchedFilter().cast<std::complex<float>>();
+
+  // Load the waveform received by the SDR
   std::vector<std::complex<float>> data = read("/home/shane/pdu_file_sink.dat");
   std::cout << data[0] << std::endl;
   std::cout << data.size() << std::endl;
-  // for (auto e : data) {
-  //   std::cout << e << std::endl;
-  // }
 
   return EXIT_SUCCESS;
 
