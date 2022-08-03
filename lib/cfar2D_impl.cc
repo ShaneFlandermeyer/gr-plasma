436469cf69ff3a5cde775c5e0488bcebcf18689b
Avery <avery.moates17@ou.edu>
2022-08-03 10:14:52 -0500

    Hard coding the recieveMessage function. Need to add new parameter, pulses per cpi.

--- lib/cfar2D_impl.cc
+++ lib/cfar2D_impl.cc
@@ -26,7 +26,22 @@ cfar2D_impl::cfar2D_impl(std::vector<int> &guard_cells, std::vector<int> &traini
                 gr::io_signature::make(0, 0, 0),
                 gr::io_signature::make(0, 0, 0)),
                 outPort(pmt::mp("Output"))
-{
+{   
+    //Setting guard window
+    guard_win.at(0) = guard_cells[0];
+    guard_win.at(1) = guard_cells[1];
+
+    //Setting training window
+    train_win.at(0) = training_cells[0];
+    train_win.at(1) = training_cells[1];
+
+    //Setting PFA
+    _pfa = pfa;
+
+    //Creating the cfar Object
+    cfarTemp = ::plasma::CFARDetector2D(guard_win, train_win, _pfa);
+
+    //Setting the message ports and handler
     message_port_register_out(outPort);
 
     message_port_register_in(pmt::mp("Input"));
@@ -38,6 +53,30 @@ cfar2D_impl::cfar2D_impl(std::vector<int> &guard_cells, std::vector<int> &traini
  * Our virtual destructor.
  */
 cfar2D_impl::~cfar2D_impl() {}
+
+void cfar2D_impl::recieveMessage(const pmt::pmt_t &msg){
+    
+    pmt::pmt_t samples;
+    if(pmt::is_pdu(msg)){
+        samples = pmt::cdr(msg);
+    }
+
+    size_t n = pmt::length(samples);
+    size_t io(0);
+
+    int num_pulse_cpi = 512; //Hard coded this. Need to add another parameter later for this.
+    int ncol = 512;
+    int nrow = n / ncol;
+    const gr_complex* in = pmt::c32vector_elements(samples, io);
+    af::array rdm(af::dim4(nrow, ncol), reinterpret_cast<const af::cfloat *>(in));
+    // Mag squared
+    rdm = af::sqrt(af::pow(rdm,2));
+    // Run CFAR
+    DetectionReport results = cfarTemp.detect(rdm);
+    // Output detections
+
+
+}
 
 
 
--- lib/cfar2D_impl.h
+++ lib/cfar2D_impl.h
@@ -8,6 +8,7 @@
 #ifndef INCLUDED_PLASMA_CFAR2D_IMPL_H
 #define INCLUDED_PLASMA_CFAR2D_IMPL_H
 
+#include <plasma_dsp/cfar2d.h>
 #include <gnuradio/plasma/cfar2D.h>
 
 namespace gr {
@@ -17,6 +18,13 @@ class cfar2D_impl : public cfar2D
 {
 private:
     const pmt::pmt_t outPort;
+
+    //Parameters for CFAR Object
+    std::array<size_t,2> guard_win;
+    std::array<size_t,2> train_win;
+    float _pfa;
+
+    ::plasma::CFARDetector2D cfarTemp;
 
     void recieveMessage(const pmt::pmt_t &msg);
 
