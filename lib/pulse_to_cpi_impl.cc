a45b64c97a77166caf1d00400acf1966195876d2
Shane Flandermeyer <shaneflandermeyer@gmail.com>
2024-08-11 10:21:21 -0500

    Cleaned up pulse to CPI block

--- lib/pulse_to_cpi_impl.cc
+++ lib/pulse_to_cpi_impl.cc
@@ -25,16 +25,16 @@ pulse_to_cpi_impl::pulse_to_cpi_impl(size_t n_pulse_cpi)
     : gr::block("pulse_to_cpi",
                 gr::io_signature::make(0, 0, 0),
                 gr::io_signature::make(0, 0, 0)),
-      d_n_pulse_cpi(n_pulse_cpi)
+      pulses_per_cpi(n_pulse_cpi)
 {
-    d_pulse_count = 0;
-    d_in_port = PMT_IN;
-    d_out_port = PMT_OUT;
+    pulse_count = 0;
+    in_port = PMT_IN;
+    out_port = PMT_OUT;
 
 
-    message_port_register_in(d_in_port);
-    message_port_register_out(d_out_port);
-    set_msg_handler(d_in_port, [this](pmt::pmt_t msg) { handle_msg(msg); });
+    message_port_register_in(in_port);
+    message_port_register_out(out_port);
+    set_msg_handler(in_port, [this](pmt::pmt_t msg) { handle_msg(msg); });
 }
 
 /*
@@ -47,43 +47,40 @@ void pulse_to_cpi_impl::handle_msg(pmt::pmt_t msg)
     pmt::pmt_t samples;
     if (pmt::is_pdu(msg)) {
         // Update input metadata
-        d_meta = pmt::dict_update(d_meta, pmt::car(msg));
+        meta = pmt::dict_update(meta, pmt::car(msg));
         samples = pmt::cdr(msg);
     } else {
         GR_LOG_WARN(d_logger, "Invalid message type")
     }
 
-    size_t samps_per_pulse = pmt::length(samples);
-    if (d_pulse_count == 0) {
-        if (d_data.size() != d_n_pulse_cpi * samps_per_pulse) {
-            d_data = std::vector<gr_complex>(d_n_pulse_cpi * samps_per_pulse);
+    size_t num_samples = pmt::length(samples);
+    if (pulse_count == 0) {
+        if (data.size() != pulses_per_cpi * num_samples) {
+            data = std::vector<gr_complex>(pulses_per_cpi * num_samples);
         }
     }
     // Store the new PDU data
-    const gr_complex* data = pmt::c32vector_elements(samples, samps_per_pulse);
-    // Insert the data into the global data vector at the current index
-    // std::copy(
-    //     data, data + samps_per_pulse, d_data.begin() + d_pulse_count * samps_per_pulse);
-    for (size_t i = 0; i < samps_per_pulse; i++) {
-        d_data[d_pulse_count * samps_per_pulse + i] = data[i];
-    }
+    const gr_complex* samples_ptr = pmt::c32vector_elements(samples, num_samples);
+    size_t start = pulse_count * num_samples;
+    // Replace the for-loop above with std::copy
+    std::copy(samples_ptr, samples_ptr + num_samples, data.begin() + start);
 
-    d_pulse_count++;
+    pulse_count++;
     // Output a PDU containing all the pulses in a column-major format
-    if (d_pulse_count == d_n_pulse_cpi) {
-        message_port_pub(d_out_port,
-                         pmt::cons(d_meta, pmt::init_c32vector(d_data.size(), d_data)));
+    if (pulse_count == pulses_per_cpi) {
+        message_port_pub(out_port,
+                         pmt::cons(meta, pmt::init_c32vector(data.size(), data)));
         // Reset the metadata
-        d_meta = pmt::make_dict();
-        d_pulse_count = 0;
+        meta = pmt::make_dict();
+        pulse_count = 0;
     }
 }
 
 void pulse_to_cpi_impl::init_meta_dict(std::string n_pulse_cpi_key)
 {
-    d_n_pulse_cpi_key = pmt::string_to_symbol(n_pulse_cpi_key);
-    d_meta = pmt::make_dict();
-    d_meta = pmt::dict_add(d_meta, d_n_pulse_cpi_key, pmt::from_long(d_n_pulse_cpi));
+    this->pulses_per_cpi_key = pmt::string_to_symbol(n_pulse_cpi_key);
+    meta = pmt::make_dict();
+    meta = pmt::dict_add(meta, pulses_per_cpi_key, pmt::from_long(pulses_per_cpi));
 }
 } /* namespace plasma */
 } /* namespace gr */
